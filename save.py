d2286d407dbc491b39fc7b66717ca65929b45c0a
Juliette Dupouy <dupouyj@ens-epua.univ-savoie.fr>
2018-12-10 11:36:20 +0100

    classe de sauvegarde : ecriture des headers

--- save.py
+++ save.py
@@ -17,13 +17,16 @@ class Save:
         return self.game.headers
     
     def fill_headers(self, values=[None,None,None,None,None,None,None]) :
+        '''
+            la fonction modifie les headers
+            :param values: 'Nom du tournoi', 'Lieu', 'Date', 'Round', 'Nom des blancs', 'Nom des noirs', 'Resultat'
+        '''
         labels = ["Event", "Site", "Date", "Round", "White", "Black", "Result"]
         i=0
         for header in labels :
             if values[i]!=None:
                 self.get_headers()[header] = values[i]
             i=i+1
-
 
 
 
@@ -39,4 +42,5 @@ board=chess.Board()
 jeu=Save(board)
 print("Type du header :", type(jeu.get_headers()))
 jeu.fill_headers()
-print(jeu.get_headers())
+#print(jeu.get_headers())
+print(jeu.game)
