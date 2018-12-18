8db211e0ce572e909ec18cafbd9d1f0d24f61d7b
NASRIchiheb <43411679+NASRIchiheb@users.noreply.github.com>
2018-12-18 08:40:50 +0100

    Polychess IA
    New polychess.py file for IA vs IA. Code isn't complete it stops at the end of the bookfish moves.

--- polychess.py
+++ polychess.py
@@ -3,6 +3,7 @@
 import chess
 import chess.svg
 from IPython.display import SVG
+import chess.polyglot
 
 #set the board to its initial position
 #corresponding to: rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
@@ -19,33 +20,42 @@ SVG(chess.svg.board(board = board, squares = squares, coordinates=True, size = 4
 
 #get all the legal moves for the current position
 moves = board.legal_moves
+print(moves)
+
 
 #how many moves are available?
 print(moves.count())
 
 ##iterate over all the moves
 for move in moves: 
+    book = chess.polyglot.open_reader("bookfish.bin")
+    
+    main_entry = book.find(board)
+    a=main_entry.move()
     
     #display the move
-    print(move)
+    print("move",a)
     
     #save the current position
     current_board = board
     
     #do the move
-    board.push(move)
+    board.push(a)
     
     #display the board
     print(board)
+    
     
     #number of black moves
     print("Black moves:" + str(board.legal_moves.count()))
     
     #undo the move
-    board.pop()
+    #board.pop()
     
     #do we have a winner?
     if (board.is_game_over()):
         print("The game is over")
         print(board.result())
     
+
+
